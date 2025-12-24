954ac4e73c873eddef3d9137594297678300b87d
2454766919 <2454766919@qq.com>
2025-12-24 15:59:36 +0800

    refactor(page): 移除未使用的Theme和Header组件
    清理首页代码，删除未使用的Theme和Header组件以简化结构

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
-import Theme from "@/components/Theme";
-import Header from "@/components/Header";
+
 export default function Home() {
   return (
     <div>
       <h1>hello page 06</h1>
-      <Theme value="dark" >
-        <Header />
-      </Theme>
     </div>
   );
 }
