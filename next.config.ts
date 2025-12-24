2e00e981d5187cb466c125b01da60bb47d5b8731
2454766919 <2454766919@qq.com>
2025-12-24 15:43:19 +0800

    feat: 初始化Next.js项目基础配置和文件结构
    添加Next.js项目基础文件，包括：
    - 项目配置文件（next.config.ts, tailwind.config.ts等）
    - 页面组件（layout.tsx, page.tsx等）
    - 静态资源（favicon.ico, svg图标等）
    - 样式和工具配置（globals.css, postcss等）
    - 基础依赖和脚本配置（package.json）

---
+++ next.config.ts
@@ -0,0 +1,17 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/400/200',
+      },
+    ]
+  }
+};
+
+export default nextConfig;
---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Theme from "@/components/Theme";
+import Header from "@/components/Header";
+export default function Home() {
+  return (
+    <div>
+      <h1>hello page 06</h1>
+      <Theme value="dark" >
+        <Header />
+      </Theme>
+    </div>
+  );
+}
