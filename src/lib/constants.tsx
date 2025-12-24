d2f822882bd1595dc3eaf62de6b048c47608111d
2454766919 <2454766919@qq.com>
2025-12-24 17:23:15 +0800

    feat: 添加商店基础布局和组件
    refactor: 重构布局和样式配置
    style: 更新全局样式和主题配置
    build: 添加tailwindcss相关依赖
    chore: 更新package.json配置

---
+++ src/lib/constants.tsx
@@ -0,0 +1,34 @@
+export const Title = "DUYI Store"
+
+export const MenuList = [
+  { href: '/search', text: 'Search' },
+  { href: '/account', text: 'Account' },
+  { href: '/cart', text: 'Cart' },
+]
+
+export const NavList = [
+  {
+    title: "Categories",
+    list: [
+      { href: '/clothing', text: 'Clothing' },
+      { href: '/audio', text: 'Audio' },
+      { href: '/furniture', text: 'Furniture' },
+    ]
+  },
+  {
+    title: "Categories",
+    list: [
+      { href: '/clothing', text: 'Clothing' },
+      { href: '/audio', text: 'Audio' },
+      { href: '/furniture', text: 'Furniture' },
+    ]
+  },
+  {
+    title: "Categories",
+    list: [
+      { href: '/clothing', text: 'Clothing' },
+      { href: '/audio', text: 'Audio' },
+      { href: '/furniture', text: 'Furniture' },
+    ]
+  }
+]
